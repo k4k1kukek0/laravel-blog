739102879b9b396c84b4fa7e714593853888f9b6
k4k1kukek0 <k4k1kukek0sm@gmail.com>
2017-03-08 10:13:03 +0700

    initial commit

---
+++ resources/views/catatan/catatan.blade.php
@@ -0,0 +1,13 @@
+git
+
+git config --global user.name "k4k1kukek0"
+git config --global user.email "k4k1kukek0sm@gmail.com"
+
+(new repository)
+cd /opt/lampp/htdocs/folder/crud
+git init
+git status  
+git add .
+git commit -m "the name of folder"
+git remote add origin https:github.com/k4k1kukek0/crud-php
+git push -u origin master
